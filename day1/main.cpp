d5038ff512ded0a4298648da42f58c3638124b8f
vladimir.rakhmanin <vrakhmanin1@gmail.com>
2025-12-01 09:40:15 +0000

    Renames Day 1 input to input.txt. Adds Day 1 Part 2 solution.

--- day1/main.cpp
+++ day1/main.cpp
@@ -3,33 +3,40 @@
 
 int main()
 {
-    std::ifstream file("inputs/part1.txt");
+    std::ifstream file("inputs/input.txt");
     std::string line;
 
-    int number_of_zeros = 0;
+    int part_one_answer = 0;
+    int part_two_answer = 0;
     int current_amount = 50;
 
     while (getline(file, line))
     {
-        const int rotation_amount = stoi(line.substr(1, line.size())) % 100;
+        const int rotation_amount_raw = stoi(line.substr(1, line.size()));
+        part_two_answer += rotation_amount_raw / 100;
+        const int rotation_amount = rotation_amount_raw % 100;
 
         if (line[0] == 'L') 
         {
             const int raw_value = current_amount - rotation_amount;
+            if (current_amount != 0 && raw_value < 0) { part_two_answer++; }
             current_amount = raw_value < 0 ? 100 + raw_value : raw_value;
         }
         else 
         {
+            if (current_amount + rotation_amount > 100) { part_two_answer++; }
             current_amount = (current_amount + rotation_amount) % 100;
         }
 
         if (current_amount == 0)
         {
-            number_of_zeros++;
+            part_one_answer++;
+            part_two_answer++;
         }
     }
 
-    std::cout << "Part One Answer: " << number_of_zeros << std::endl;
+    std::cout << "Part One Answer: " << part_one_answer << std::endl;
+    std::cout << "Part Two Answer: " << part_two_answer << std::endl;
 
     return 0;
 }
