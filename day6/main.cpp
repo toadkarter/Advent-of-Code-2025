d9c66ddf0e828d3eefe5e7a590ce3b45d57bea2e
toadkarter <vrakhmanin1@gmail.com>
2025-12-06 10:46:14 +0000

    Updates parsing to take whitespace into account.

--- day6/main.cpp
+++ day6/main.cpp
@@ -10,45 +10,74 @@ struct Problem
 {
     std::vector<long> operands {};
     char action = ' ';
+
+    std::vector<std::string> part_two_input;
 };
 
 int main()
 {
-    std::ifstream file("inputs/input.txt");
+    std::ifstream file("inputs/sample.txt");
     std::string line;
 
     long long part_one_answer = 0;
     long long part_two_answer = 0;
 
-    std::vector<Problem> problems;
+    std::vector<Problem> problems{};
 
     while (getline(file, line))
     {
-        // This monstrosity apparently lets you strip out whitespace. Good to know!
-        std::istringstream ss(line);
-        std::vector<std::string> numbers{std::istream_iterator<std::string>(ss), std::istream_iterator<std::string>()};
-
-        if (problems.size() == 0)
+        if (line[0] == '*' || line[0] == '/' || line[0] == '+' || line[0] == '-')
         {
-            problems.resize(numbers.size());
-        }
-
-        bool is_action = false;
-
-        if (numbers[0] == "*" || numbers[0] == "/" || numbers[0] == "+" || numbers[0] == "-")
-        {
-            is_action = true;
-        }
-
-        for (int i = 0; i < numbers.size(); i++)
-        {
-            if (is_action)
+            // This monstrosity apparently lets you strip out whitespace. Good to know!
+            std::istringstream ss(line);
+            std::vector<std::string> operands{std::istream_iterator<std::string>(ss), std::istream_iterator<std::string>()};
+            for (int i = 0; i < problems.size(); i++)
             {
-                problems[i].action = numbers[i][0];
+                problems[i].action = operands[i][0];
             }
-            else
+        }
+        else
+        {
+            int current_num_index = 0;
+            int prev_pos = 0;
+            int current_pos = 0;
+
+            while (current_pos < line.size() - 1)
             {
-                problems[i].operands.push_back(stol(numbers[i]));
+                if (line[0] == ' ')
+                {
+                    while (line[current_pos] == ' ')
+                    {
+                        current_pos++;
+                    }
+
+                    prev_pos = current_pos;
+                }
+
+                while (current_pos < line.size() - 1 && line[current_pos] != ' ')
+                {
+                    current_pos++;
+                }
+
+                while (current_pos < line.size() - 1 && line[current_pos] == ' ')
+                {
+                    current_pos++;
+                }
+
+                std::string operand_with_whitespace = line.substr(prev_pos, current_pos - prev_pos);
+                if (current_num_index >= problems.size())
+                {
+                    problems.push_back(Problem{});
+                }
+
+                problems[current_num_index].part_two_input.push_back(operand_with_whitespace);
+
+                std::string::iterator strip = std::remove(operand_with_whitespace.begin(), operand_with_whitespace.end(), ' ');
+                operand_with_whitespace.erase(strip, operand_with_whitespace.end());
+                problems[current_num_index].operands.push_back(stol(operand_with_whitespace));
+
+                prev_pos = current_pos;
+                current_num_index++;
             }
         }
     }
